c4d40fd239e48a641717fcc3e1531b899d463329
simk395 <simon.kong95@gmail.com>
2019-04-17 13:03:53 -0400

    feed complete

--- got-front/src/component/Feed.js
+++ got-front/src/component/Feed.js
@@ -6,19 +6,19 @@ export class Feed extends Component {
   render() {
       const { posts, profiles } = this.props
       let postArr = []
-      const recentPosts = posts.slice(Math.max(posts.length - 5, 0)).reverse()
+      const recentPosts = posts.slice(Math.max(posts.length - 7, 0)).reverse()
       for(let i = 0; i < recentPosts.length; i++){
         let profile = profiles.find(profile => recentPosts[i].user_id === profile.id)
-        const body = <div >
-                      <Link to={`/forum/${recentPosts[i].category_id}/${recentPosts[i].id}`}>{recentPosts[i].title}</Link>
-                      <p>By {profile ? <Link to={`/profile/${profile.username}`}>{profile.username}</Link> : "Anonymous"}</p>
+        const body = <div className="feed_body">
+                      <Link className="feed_title" to={`/forum/${recentPosts[i].category_id}/${recentPosts[i].id}`}>{recentPosts[i].title}</Link>
+                      <p className="feed_author">By {profile ? <Link to={`/profile/${profile.username}`}>{profile.username}</Link> : "Anonymous"}</p>
                       <p>Posted: {recentPosts[i].created_at}</p>
                     </div>;
-        i % 2 === 0 ? postArr.push( <li className="fb_body odd">{ body }</li>) : postArr.push(<li className="fb_body even">{ body } </li>)
+        postArr.push(<li className="feed_body_list">{ body } </li>)
       }
     return (
       <div className="feed_main">
-        <h4>Most Recent Posts</h4>
+        <h2 className="feed_heading">Most Recent Posts</h2>
         {postArr.map(post => post)}
       </div>
     )
